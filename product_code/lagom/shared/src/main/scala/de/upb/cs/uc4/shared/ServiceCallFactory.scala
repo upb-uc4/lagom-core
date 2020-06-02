5a6ab7ef585d5860cf433f2a9844de9dbfabb0ed
dbuderus <dbuderus@mail.uni-paderborn.de>
2020-06-02 14:02:51 +0200

    Upgrade logged wrapper to org.slf4j.Logger

--- product_code/lagom/shared/src/main/scala/de/upb/cs/uc4/shared/ServiceCallFactory.scala
+++ product_code/lagom/shared/src/main/scala/de/upb/cs/uc4/shared/ServiceCallFactory.scala
@@ -1,8 +1,11 @@
 package de.upb.cs.uc4.shared
 
 import com.lightbend.lagom.scaladsl.server.ServerServiceCall
+import org.slf4j.{Logger, LoggerFactory}
 
 object ServiceCallFactory {
+
+  private final val log: Logger = LoggerFactory.getLogger("Shared")
 
   /** Wraps a [[com.lightbend.lagom.scaladsl.api.ServiceCall]] into a Logger.
     * Logs the header method and header uri of any incoming call.
@@ -12,7 +15,7 @@ object ServiceCallFactory {
     */
   def logged[Request, Response](serviceCall: ServerServiceCall[Request, Response]): ServerServiceCall[Request, Response] =
     ServerServiceCall.compose { requestHeader =>
-      println(s"Received ${requestHeader.method} ${requestHeader.uri}")
+      log.info("Received {} {}", requestHeader.method, requestHeader.uri)
       serviceCall
     }
 }
