2b6217fff20b69a766e090df081633b639a229c3
user <mgeuchen@mail.uni-paderborn.de>
2020-06-11 15:56:44 +0200

    Add debug output.
    Co-authored-by: Marie-Christin Oczko <mco33415@gmail.com>
    Co-authored-by: Niko Bergemann <Niko.Bergemann@web.de>
    Co-authored-by: Niklas Doppelstein <n.doppelstein@web.de>
    Co-authored-by: Steffen Luis <sluis1@mail.uni-paderborn.de>

--- product_code/hyperledger/dev_network/scripts/chaincode.sh
+++ product_code/hyperledger/dev_network/scripts/chaincode.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
 echo "Start sleep"
-sleep 15s
+sleep 30000s #15s
 echo "Finish sleep"
 
 pushd /opt/gopath/src/chaincode
 
-./../gradlew installDist
+echo "############################################################################################"
+echo "#                                   COMPILING CHAINCODE                                    #"
+echo "############################################################################################"
 
-CORE_CHAINCODE_ID_NAME=mycc:0 CORE_PEER_TLS_ENABLED=false ./../gradlew run
+/opt/gopath/src/gradlew installDist
+#./gradlew installDist
+
+echo "############################################################################################"
+echo "#                                   CHAINCODE COMPILED                                     #"
+echo "############################################################################################"
+
+echo "############################################################################################"
+echo "#                                   STARTING CHAINCODE                                     #"
+echo "############################################################################################"
+
+CORE_CHAINCODE_ID_NAME=mycc:0 CORE_PEER_TLS_ENABLED=false /opt/gopath/src/gradlew run
+#CORE_CHAINCODE_ID_NAME=mycc:0 CORE_PEER_TLS_ENABLED=false ./gradlew run
+
 
 popd
