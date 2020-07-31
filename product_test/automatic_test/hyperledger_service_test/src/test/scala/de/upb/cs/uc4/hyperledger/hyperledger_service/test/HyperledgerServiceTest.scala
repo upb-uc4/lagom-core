751459140910f7058efdb91fe0858e6b5156da95
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-07-31 13:38:09 +0200

    finalize merge

--- product_test/automatic_test/hyperledger_service_test/src/test/scala/de/upb/cs/uc4/hyperledger/hyperledger_service/test/HyperledgerServiceTest.scala
+++ product_test/automatic_test/hyperledger_service_test/src/test/scala/de/upb/cs/uc4/hyperledger/hyperledger_service/test/HyperledgerServiceTest.scala
@@ -9,11 +9,9 @@ import org.scalatest.matchers.should.Matchers
 import com.lightbend.lagom.scaladsl.testkit.ServiceTest
 import de.upb.cs.uc4.course.model.Course
 import de.upb.cs.uc4.hyperledger.impl._
-import de.upb.cs.uc4.hyperledger.exceptions._
 import de.upb.cs.uc4.hyperledger.api._
-import de.upb.cs.uc4.shared.client.exceptions.CustomException
 import de.upb.cs.uc4.hyperledger.exceptions.TransactionErrorException
-import de.upb.cs.uc4.shared.client.CustomException
+import de.upb.cs.uc4.shared.client.exceptions.CustomException
 import de.upb.cs.uc4.test_resources._
 import play.api.libs.json.Json
 
