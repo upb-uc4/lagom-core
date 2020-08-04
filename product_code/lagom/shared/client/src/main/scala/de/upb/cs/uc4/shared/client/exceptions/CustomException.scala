88c801a7e26b9762cdd7863e7c529eb903a1774b
Daniel Helmert <dhelmert@mail.upb.de>
2020-08-04 16:16:20 +0200

    Add standard exceptions to CustomExceptions, add new constructor without TransportErrorCode
    Co-authored-by: Luca Schäfers <lrschaef@mail.uni-paderborn.de>

--- product_code/lagom/shared/client/src/main/scala/de/upb/cs/uc4/shared/client/exceptions/CustomException.scala
+++ product_code/lagom/shared/client/src/main/scala/de/upb/cs/uc4/shared/client/exceptions/CustomException.scala
@@ -8,6 +8,25 @@ class CustomException(errorCode: TransportErrorCode, possibleErrorResponse: Cust
   def this(errorCode: TransportErrorCode, possibleErrorResponse: CustomError) =
     this(errorCode, possibleErrorResponse, null)
 
+  def this(errorCode: Int, possibleErrorResponse: CustomError) =
+    this (TransportErrorCode(errorCode, 1003, "Error"), possibleErrorResponse)
+
+  //400
+  val DeserializationError = new CustomException(400, GenericError("deserialization error"))
+  //401
+  val AuthorizationError = new CustomException(401, GenericError("authorization error"))
+  //403
+  val NotEnoughPrivileges = new CustomException(403, GenericError("not enough privileges"))
+  val OwnerMismatch = new CustomException(403, GenericError("owner mismatch"))
+  //404
+  val NotFound = new CustomException(404, GenericError("key not found"))
+  //409
+  val Duplicate = new CustomException(409, GenericError("key duplicate"))
+  //422
+  val PathParameterMismatch = new CustomException(422, GenericError("path parameter mismatch"))
+  //500
+  val InternalServerError = new CustomException(500, GenericError("Server Error"))
+  val InternalDeserializationError = new CustomException(500, GenericError("undeserializable exception"))
 
   def getErrorCode: TransportErrorCode = {
     errorCode
