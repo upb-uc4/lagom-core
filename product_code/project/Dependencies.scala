b0afd7012f701b2a7d9ce694823b32c654276b89
dbuderus <dbuderus@mail.uni-paderborn.de>
2020-09-28 11:39:47 +0200

    Fix Dependencies

--- product_code/project/Dependencies.scala
+++ product_code/project/Dependencies.scala
@@ -6,6 +6,7 @@ object Dependencies {
   val macwire = "com.softwaremill.macwire" %% "macros" % "2.3.3" % "provided"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.0" % Test
   val flexmark = "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test
+  val playTest = "com.typesafe.play" %% "play-test" % "2.8.0" % Test
   val guava = "com.google.guava" % "guava" % "29.0-jre"
   val akkaDiscoveryKubernetes = "com.lightbend.akka.discovery" %% "akka-discovery-kubernetes-api" % "1.0.8"
   val postgresDriver = "org.postgresql" % "postgresql" % "42.2.8"
@@ -26,6 +27,7 @@ object Dependencies {
     filters,
     macwire,
     scalaTest,
+    playTest,
     flexmark,
     janino
   )
