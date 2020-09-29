e13f3cd78dc34fc901e3ce0db531ef9597977c9a
Luca Schäfers <lrschaef@mail.uni-paderborn.de>
2020-09-29 11:56:04 +0200

    Implement csr
    Co-authored-by: David Buderus <dbuderus@mail.uni-paderborn.de>
    Co-authored-by: Lars Klenner <lklenner@mail.uni-paderborn.de>
    Co-authored-by: Daniel Helmert <dhelmert@mail.uni-paderborn.de>
    Co-authored-by: Robin Helmert <rhelmert@mail.uni-paderborn.de

--- product_code/certificate_service/impl/src/main/scala/de/upb/cs/uc4/certificate/impl/CertificateServiceImpl.scala
+++ product_code/certificate_service/impl/src/main/scala/de/upb/cs/uc4/certificate/impl/CertificateServiceImpl.scala
@@ -1,24 +1,24 @@
 package de.upb.cs.uc4.certificate.impl
 
-import akka.cluster.sharding.typed.scaladsl.{ ClusterSharding, EntityRef }
+import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, EntityRef}
 import akka.util.Timeout
-import akka.{ Done, NotUsed }
+import akka.{Done, NotUsed}
 import com.lightbend.lagom.scaladsl.api.ServiceCall
-import com.lightbend.lagom.scaladsl.api.transport.{ MessageProtocol, ResponseHeader }
+import com.lightbend.lagom.scaladsl.api.transport.{MessageProtocol, ResponseHeader}
 import com.lightbend.lagom.scaladsl.server.ServerServiceCall
 import com.typesafe.config.Config
 import de.upb.cs.uc4.authentication.model.AuthenticationRole
 import de.upb.cs.uc4.certificate.api.CertificateService
 import de.upb.cs.uc4.certificate.impl.actor.CertificateState
-import de.upb.cs.uc4.certificate.impl.commands.{ CertificateCommand, GetCertificateUser, SetCertificateAndKey }
-import de.upb.cs.uc4.certificate.model.{ EncryptedPrivateKey, JsonCertificate, JsonEnrollmentId, PostMessageCSR }
+import de.upb.cs.uc4.certificate.impl.commands.{CertificateCommand, GetCertificateUser, SetCertificateAndKey}
+import de.upb.cs.uc4.certificate.model.{EncryptedPrivateKey, JsonCertificate, JsonEnrollmentId, PostMessageCSR}
 import de.upb.cs.uc4.hyperledger.HyperledgerAdminParts
 import de.upb.cs.uc4.hyperledger.utilities.EnrollmentManager
-import de.upb.cs.uc4.shared.client.exceptions.{ CustomException, DetailedError, ErrorType }
+import de.upb.cs.uc4.shared.client.exceptions.{CustomException, DetailedError, ErrorType}
 import de.upb.cs.uc4.shared.server.ServiceCallFactory._
-import de.upb.cs.uc4.shared.server.messages.{ Accepted, Confirmation }
+import de.upb.cs.uc4.shared.server.messages.{Accepted, Confirmation}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
 /** Implementation of the UserService */
@@ -37,23 +37,20 @@ class CertificateServiceImpl(clusterSharding: ClusterSharding)
     (authUsername, _) =>
       ServerServiceCall { (_, pmcsrRaw) =>
         // One can only set his own certificate
-        if (authUsername != username){
+        if (authUsername != username) {
           throw CustomException.OwnerMismatch
         }
 
         // Validate the CSR
         val validationErrors = pmcsrRaw.validate
-        if (validationErrors.nonEmpty){
+        if (validationErrors.nonEmpty) {
           throw new CustomException(422, DetailedError(ErrorType.Validation, validationErrors))
         }
 
-        entityRef(username).ask[(Option[String], Option[String], Option[String], Option[EncryptedPrivateKey])](replyTo => GetCertificateUser(replyTo))
-          .flatMap {
+        getCertificateUser(username).flatMap {
             case (Some(enrollmentId), Some(enrollmentSecret), _, _) =>
-              //TODO use the pmcsrRaw info to enroll
-              EnrollmentManager.enroll(caURL, tlsCert, walletPath, enrollmentId, enrollmentSecret, organisationId)
-              //TODO add certificate to state, or fetch directly from HL
-              entityRef(username).ask[Confirmation](replyTo => SetCertificateAndKey("cert", pmcsrRaw.encryptedPrivateKey, replyTo)).map{
+              val certificate = EnrollmentManager.enrollSecure(caURL, tlsCert, enrollmentId, enrollmentSecret, pmcsrRaw.certificateSigningRequest)
+              entityRef(username).ask[Confirmation](replyTo => SetCertificateAndKey(certificate, pmcsrRaw.encryptedPrivateKey, replyTo)).map {
                 case Accepted => (ResponseHeader(202, MessageProtocol.empty, List()), Done)
                 case _ => throw CustomException.InternalServerError
               }
@@ -67,8 +64,7 @@ class CertificateServiceImpl(clusterSharding: ClusterSharding)
   /** Returns the certificate of the given user */
   override def getCertificate(username: String): ServiceCall[NotUsed, JsonCertificate] =
     ServerServiceCall { (_, _) =>
-      entityRef(username).ask[(Option[String], Option[String], Option[String], Option[EncryptedPrivateKey])](replyTo => GetCertificateUser(replyTo))
-        .map {
+      getCertificateUser(username).map {
           case (_, _, Some(certificate), _) =>
             (ResponseHeader(200, MessageProtocol.empty, List()), JsonCertificate(certificate))
           case _ =>
@@ -81,12 +77,11 @@ class CertificateServiceImpl(clusterSharding: ClusterSharding)
     (authUsername, _) =>
       ServerServiceCall { (_, _) =>
 
-        if (authUsername != username){
+        if (authUsername != username) {
           throw CustomException.OwnerMismatch
         }
 
-        entityRef(username).ask[(Option[String], Option[String], Option[String], Option[EncryptedPrivateKey])](replyTo => GetCertificateUser(replyTo))
-          .map {
+        getCertificateUser(username).map {
             case (Some(id), _, _, _) =>
               (ResponseHeader(200, MessageProtocol.empty, List()), JsonEnrollmentId(id))
             case _ =>
@@ -101,12 +96,11 @@ class CertificateServiceImpl(clusterSharding: ClusterSharding)
     (authUsername, _) =>
       ServerServiceCall { (_, _) =>
 
-        if (authUsername != username){
+        if (authUsername != username) {
           throw CustomException.OwnerMismatch
         }
 
-        entityRef(username).ask[(Option[String], Option[String], Option[String], Option[EncryptedPrivateKey])](replyTo => GetCertificateUser(replyTo))
-          .map {
+        getCertificateUser(username).map {
             case (_, _, _, Some(key)) =>
               (ResponseHeader(200, MessageProtocol.empty, List()), key)
             case _ =>
@@ -114,6 +108,10 @@ class CertificateServiceImpl(clusterSharding: ClusterSharding)
           }
       }
   }
+
+  /** Helper method for getting the actor that corresponds to the given username */
+  private def getCertificateUser(username: String): Future[(Option[String], Option[String], Option[String], Option[EncryptedPrivateKey])] =
+    entityRef(username).ask[(Option[String], Option[String], Option[String], Option[EncryptedPrivateKey])](replyTo => GetCertificateUser(replyTo))
 
   /** This Methods needs to allow a GET-Method */
   override def allowVersionNumber: ServiceCall[NotUsed, Done] = allowedMethodsCustom("GET")
