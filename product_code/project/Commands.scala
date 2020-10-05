4b9256ab3e58dc33a0fa310c6967a6982ce70c7d
dbuderus <dbuderus@mail.uni-paderborn.de>
2020-10-05 13:06:22 +0200

    Add Version Check at Deploymnet
    Co-authored-by: Lars Klenner <lklenner@mail.uni-paderborn.de>

--- product_code/project/Commands.scala
+++ product_code/project/Commands.scala
@@ -1,25 +1,59 @@
-import sbt.{ Command, Project, State }
+import sbt.internal.util.ConsoleLogger
+import sbt.{ Command, State }
 
 object Commands {
 
-  val dependenciesCheck: Command = Command.single("dependenciesCheck") { (state: State, project) =>
-    import state._
+  val versionCheck: Command = Command.single("versionCheck") { (state: State, tag: String) =>
 
-    println("sbt version: " + configuration.provider.id.version)
-    println("Scala version (for sbt): " + configuration.provider.scalaProvider.version)
-    println()
+    lazy val logger = ConsoleLogger()
+    val tagRegex = """([A-Za-z]*)-(v[0-9]+\.[0-9]+\.[0-9]+)""".r
 
-    val extracted = Project.extract(state)
-    import extracted._
-    println(state)
-    println("Current build: " + currentRef.build)
-    println("Current project: " + currentRef.project)
-    println("Current subs: " + currentRef.project)
-    println("Original setting count: " + session.original.size)
-    println("Session setting count: " + session.append.size)
+    try {
+      val tagRegex(service, version) = tag
 
-    state
+      try {
+        if (compare(version, Version(s"${service}_service")) == 0) {
+          logger.info("The version matches the tag.")
+          state
+        } else {
+          logger.err(s"The version of $service is ${Version(s"${service}_service")}, but was tagged with $version.")
+          state.fail
+        }
+      }
+      catch {
+        case _: NoSuchElementException =>
+          logger.err(s"The service $service is not defined in the build.")
+          state.fail
+      }
+    }
+    catch {
+      case _: Throwable =>
+        logger.err(s"The tag $tag is malformed.")
+        state.fail
+    }
   }
 
-  val all = Seq(dependenciesCheck)
+  /** Compares two version strings.
+    *
+    * @param   version1 the first version to be compared.
+    * @param   version2 the second version to be compared.
+    * @return the value 0 if version1 is
+    *         equal to version2; a value less than
+    *         0 if version1 is smaller
+    *         than version2 and a value greater
+    *         than 0 if version1 is
+    *         greater than version2.
+    */
+  private def compare(version1: String, version2: String): Int = {
+    val Array(major1, minor1, patch1) = version1.substring(1).split("\\.").map(_.toInt)
+    val Array(major2, minor2, patch2) = version2.substring(1).split("\\.").map(_.toInt)
+
+    val majorDif = major1.compareTo(major2)
+    val minorDif = minor1.compareTo(minor2)
+    val patchDif = patch1.compareTo(patch2)
+
+    if (majorDif == 0) if (minorDif == 0) patchDif else minorDif else majorDif
+  }
+
+  val all = Seq(versionCheck)
 }
