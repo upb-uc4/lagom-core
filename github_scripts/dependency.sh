e2da89fa862a14f5b2106abd904e9fc65a8c739c
dbuderus <dbuderus@mail.uni-paderborn.de>
2020-10-06 11:29:56 +0200

    With parameter

--- github_scripts/dependency.sh
+++ github_scripts/dependency.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-  
-service=${{ github.event.inputs.target_service }}
-if sbt "dependencyCheck $service"
+
+if sbt "dependencyCheck $1"
 then
   echo "Failure=0" >> $GITHUB_ENV
 else
   echo "Failure=1" >> $GITHUB_ENV
   message=`cat target/dependencyCheck.txt`
   echo "FailureMessage=$message" >> $GITHUB_ENV
-  echo "ServiceName=$service" >> $GITHUB_ENV
+  echo "ServiceName=$1" >> $GITHUB_ENV
 fi
