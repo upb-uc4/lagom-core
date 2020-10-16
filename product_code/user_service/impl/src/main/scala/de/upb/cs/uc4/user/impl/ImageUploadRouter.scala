410cdb0464c52696b72d8073b0b9417dd6258f2a
dbuderus <dbuderus@mail.uni-paderborn.de>
2020-10-16 14:53:26 +0200

    Fix Wrong ExceptionHandling in UploadRouter

--- product_code/user_service/impl/src/main/scala/de/upb/cs/uc4/user/impl/ImageUploadRouter.scala
+++ product_code/user_service/impl/src/main/scala/de/upb/cs/uc4/user/impl/ImageUploadRouter.scala
@@ -2,7 +2,7 @@ package de.upb.cs.uc4.user.impl
 
 import akka.stream.Materializer
 import com.lightbend.lagom.scaladsl.api.transport.RequestHeader
-import de.upb.cs.uc4.shared.client.exceptions.{ ErrorType, GenericError, UC4CriticalException }
+import de.upb.cs.uc4.shared.client.exceptions.{ ErrorType, GenericError, UC4Exception }
 import play.api.mvc.{ DefaultActionBuilder, PlayBodyParsers, Result, Results }
 import play.api.routing.Router
 import play.api.routing.sird._
@@ -37,8 +37,8 @@ class ImageUploadRouter(action: DefaultActionBuilder, parser: PlayBodyParsers, u
   }
 
   private def handleException: PartialFunction[Throwable, Result] = {
-    case customException: UC4CriticalException =>
-      new Results.Status(customException.errorCode.http)(customException.possibleErrorResponse)
+    case uc4Exception: UC4Exception =>
+      new Results.Status(uc4Exception.errorCode.http)(uc4Exception.possibleErrorResponse)
     case _: Exception =>
       Results.InternalServerError(GenericError(ErrorType.InternalServer))
   }
