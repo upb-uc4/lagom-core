9b5157ca3181ddd4f06a3c6ccbe71af2f041fe9a
dbuderus <dbuderus@mail.uni-paderborn.de>
2020-10-19 12:13:34 +0200

    Adjust Logback Files
    Co-authored-by: Lars Klenner <lklenner@mail.uni-paderborn.de>
    Co-authored-by: Robin Helmert <rhelmert@mail.uni-paderborn.de>

--- product_code/user_service/impl/src/test/scala/de/upb/cs/uc4/user/impl/UserStateSpec.scala
+++ product_code/user_service/impl/src/test/scala/de/upb/cs/uc4/user/impl/UserStateSpec.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.persistence.typed.PersistenceId
-import de.upb.cs.uc4.shared.server.messages.{ Accepted, Confirmation, Rejected, RejectedWithError }
+import de.upb.cs.uc4.shared.server.messages.{ Accepted, Confirmation, RejectedWithError }
 import de.upb.cs.uc4.user.impl.actor.UserBehaviour
 import de.upb.cs.uc4.user.impl.commands._
 import de.upb.cs.uc4.user.model.user._
