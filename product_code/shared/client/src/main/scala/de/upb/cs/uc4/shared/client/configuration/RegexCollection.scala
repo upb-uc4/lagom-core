4dc0733ccb13e0e0f9764b08a4547bf9eb2cc385
Luca Schäfers <lrschaef@mail.uni-paderborn.de>
2020-10-22 12:20:58 +0200

    Add new mail RegEx

--- product_code/shared/client/src/main/scala/de/upb/cs/uc4/shared/client/configuration/RegexCollection.scala
+++ product_code/shared/client/src/main/scala/de/upb/cs/uc4/shared/client/configuration/RegexCollection.scala
@@ -26,12 +26,12 @@ object RegexCollection {
   object User {
     val generalRegex: Regex = """[\s\S]{0,200}""".r // Allowed characters for general strings TBD
     val usernameRegex: Regex = """[a-zA-Z0-9-.]{4,16}""".r
-    val mailRegex: Regex = """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])""".r
+    val mailRegex: Regex = """([a-z0-9!#$%&'*+/=?^_`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*)@(([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?)""".r
     val phoneNumberRegex: Regex = """\+[0-9]{1,30}""".r
   }
 
   object Address {
-    val houseNumberRegex: Regex = """[1-9][0-9]{0,4}([a-zA-Z]([0-9]|-[a-zA-Z]){0,1}){0,1}""".r
+    val houseNumberRegex: Regex = """[1-9][0-9]{0,4}([a-zA-Z]([0-9]|-[a-zA-Z])?)?""".r
     val nameRegex: Regex = """[a-zA-Z.,\s\u00c4\u00e4\u00d6\u00f6\u00dc\u00fc\u00df0-9-]{1,50}""".r
   }
 
