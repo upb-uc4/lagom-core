4389768ccbcb152ec1e7fde25892574bdf1a94b6
dbuderus <dbuderus@mail.uni-paderborn.de>
2020-10-22 11:56:01 +0200

    Adjust to new credentials system

--- deployment/full_start.sh
+++ deployment/full_start.sh
@@ -6,5 +6,19 @@ if [ $# -eq 0 ]
     exit -1
 fi
 
+pushd ./lagom-core/deployment
 ./start_cluster.sh $1
+popd
+
+pushd ./hlf-network
+if [ $# -eq 2 ]
+then
+	./deploy.sh -c /data/$1/hyperledger/ -b $2
+else
+	./deploy.sh -c /data/$1/hyperledger/
+fi
+popd
+
+pushd ./lagom-core/deployment
 ./deploy_on_cluster.sh
+popd
