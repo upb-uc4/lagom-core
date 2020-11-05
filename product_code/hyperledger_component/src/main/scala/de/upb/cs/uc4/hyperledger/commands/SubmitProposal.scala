1d5d7a679b51523f9a0f2dbcd3a7020e098853e2
Luca Schäfers <lrschaef@mail.uni-paderborn.de>
2020-11-05 11:23:05 +0100

    Make adjustments to support new HL-API v0.11.5

--- product_code/hyperledger_component/src/main/scala/de/upb/cs/uc4/hyperledger/commands/SubmitProposal.scala
+++ product_code/hyperledger_component/src/main/scala/de/upb/cs/uc4/hyperledger/commands/SubmitProposal.scala
@@ -5,9 +5,9 @@ import com.google.protobuf.ByteString
 import de.upb.cs.uc4.shared.client.SignedTransactionProposal
 import de.upb.cs.uc4.shared.server.messages.Confirmation
 
-case class SubmitProposal(unsignedProposal: Array[Byte], signature: ByteString, replyTo: ActorRef[Confirmation]) extends HyperledgerWriteCommand
+case class SubmitProposal(unsignedProposal: Array[Byte], signature: Array[Byte], replyTo: ActorRef[Confirmation]) extends HyperledgerWriteCommand
 
 object SubmitProposal {
   def apply(proposal: SignedTransactionProposal, replyTo: ActorRef[Confirmation]) =
-    new SubmitProposal(proposal.unsignedProposalAsByteArray, proposal.signatureAsByteString, replyTo)
+    new SubmitProposal(proposal.unsignedProposalAsByteArray, proposal.signatureAsByteArray, replyTo)
 }
