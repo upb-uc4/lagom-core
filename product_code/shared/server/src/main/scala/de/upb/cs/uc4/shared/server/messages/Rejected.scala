23daeb19bb998717d70922a88f0f707f1362d2e3
dbuderus <dbuderus@mail.uni-paderborn.de>
2020-11-06 11:12:18 +0100

    Update Comment of Rejected

--- product_code/shared/server/src/main/scala/de/upb/cs/uc4/shared/server/messages/Rejected.scala
+++ product_code/shared/server/src/main/scala/de/upb/cs/uc4/shared/server/messages/Rejected.scala
@@ -3,7 +3,7 @@ package de.upb.cs.uc4.shared.server.messages
 import de.upb.cs.uc4.shared.client.exceptions.UC4Error
 import play.api.libs.json.{ Format, Json }
 
-/** RejectedWithError version of the Confirmation */
+/** Rejected version of the Confirmation */
 case class Rejected(statusCode: Int, reason: UC4Error) extends Confirmation
 
 object Rejected {
