bdd02a9bf15840ad5a645c654464d99c245d3b6a
Lars Dietrich Klenner <lklenner@mail.uni-paderborn.de>
2020-11-11 16:38:04 +0100

    Bump version of configuration service

--- product_code/project/Version.scala
+++ product_code/project/Version.scala
@@ -4,7 +4,7 @@ object Version {
   private val versions: Map[String, String] = Map(
     "authentication_service" -> "v0.12.0",
     "certificate_service" -> "v0.12.0",
-    "configuration_service" -> "v0.12.0",
+    "configuration_service" -> "v0.12.1",
     "course_service" -> "v0.12.0",
     "hyperledger_api" -> "0.11.5",
     "matriculation_service" -> "v0.12.0",
