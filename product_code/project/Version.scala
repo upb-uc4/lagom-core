e89a435579607d9ef5628c1bcfb4238f2f0404d2
Daniel Helmert <dhelmert@mail.upb.de>
2020-11-20 15:09:47 +0100

    Update changelog, bump version of examreg

--- product_code/project/Version.scala
+++ product_code/project/Version.scala
@@ -6,7 +6,7 @@ object Version {
     "certificate_service" -> "v0.12.0",
     "configuration_service" -> "v0.12.1",
     "course_service" -> "v0.12.2",
-    "examreg_service" -> "v0.12.2",
+    "examreg_service" -> "v0.12.3",
     "hyperledger_api" -> "0.11.5",
     "matriculation_service" -> "v0.12.1",
     "user_service" -> "v0.12.0"
