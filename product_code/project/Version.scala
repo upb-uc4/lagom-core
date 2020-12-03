ef0899ee6690aa09d79c4be8cc1f8ac34f1aa315
Luca Schäfers <lrschaef@mail.uni-paderborn.de>
2020-12-03 11:58:51 +0100

    Revert incorrect version bump

--- product_code/project/Version.scala
+++ product_code/project/Version.scala
@@ -8,7 +8,7 @@ object Version {
     "course_service" -> "v0.13.1",
     "examreg_service" -> "v0.12.3",
     "hyperledger_api" -> "0.11.5",
-    "matriculation_service" -> "v0.13.2",
+    "matriculation_service" -> "v0.13.1",
     "user_service" -> "v0.13.3"
   )
 
