1911499447b82b4fae76be9786d2174edd336a18
David Buderus <dbuderus@mail.uni-paderborn.de>
2020-12-10 16:01:31 +0100

    Bump versions

--- product_code/project/Version.scala
+++ product_code/project/Version.scala
@@ -2,14 +2,14 @@
 object Version {
 
   private val versions: Map[String, String] = Map(
-    "authentication_service" -> "v0.13.1",
-    "certificate_service" -> "v0.13.1",
-    "configuration_service" -> "v0.13.1",
-    "course_service" -> "v0.13.1",
-    "examreg_service" -> "v0.13.1",
+    "authentication_service" -> "v0.14.1",
+    "certificate_service" -> "v0.14.1",
+    "configuration_service" -> "v0.14.1",
+    "course_service" -> "v0.14.1",
+    "examreg_service" -> "v0.14.1",
     "hyperledger_api" -> "014.1",
-    "matriculation_service" -> "v0.13.2",
-    "user_service" -> "v0.13.3"
+    "matriculation_service" -> "v0.14.1",
+    "user_service" -> "v0.14.1"
   )
 
   /** Returns the version of a project
