b9f47fcd8a9eb41c3ec040acb5d34f495173cab0
Luca Schäfers <lrschaef@mail.uni-paderborn.de>
2020-12-11 12:11:15 +0100

    Add calls to the other services
    Co-authored-by: David Buderus <dbuderus@mail.uni-paderborn.de>
    Co-authored-by: Lars Klenner <lklenner@mail.uni-paderborn.de>
    Co-authored-by: Daniel Helmert <dhelmert@mail.uni-paderborn.de>
    Co-authored-by: Robin Helmert <rhelmert@mail.uni-paderborn.de>
    Co-authored-by: Jonas Thiele <JonasThi97@gmail.com>

--- product_code/report_service/impl/src/main/scala/de/upb/cs/uc4/report/impl/ReportServiceImpl.scala
+++ product_code/report_service/impl/src/main/scala/de/upb/cs/uc4/report/impl/ReportServiceImpl.scala
@@ -4,25 +4,26 @@ import akka.cluster.sharding.typed.scaladsl.{ ClusterSharding, EntityRef }
 import akka.util.Timeout
 import akka.{ Done, NotUsed }
 import com.lightbend.lagom.scaladsl.api.ServiceCall
-import com.lightbend.lagom.scaladsl.api.transport._
+import com.lightbend.lagom.scaladsl.api.transport.{ MessageProtocol, ResponseHeader }
 import com.lightbend.lagom.scaladsl.server.ServerServiceCall
 import com.typesafe.config.Config
 import de.upb.cs.uc4.authentication.model.AuthenticationRole
 import de.upb.cs.uc4.certificate.api.CertificateService
 import de.upb.cs.uc4.course.api.CourseService
+import de.upb.cs.uc4.course.model.Course
 import de.upb.cs.uc4.matriculation.api.MatriculationService
+import de.upb.cs.uc4.matriculation.model.ImmatriculationData
 import de.upb.cs.uc4.report.api.ReportService
 import de.upb.cs.uc4.report.impl.actor.ReportState
 import de.upb.cs.uc4.report.impl.commands.ReportCommand
 import de.upb.cs.uc4.shared.client.exceptions._
 import de.upb.cs.uc4.shared.server.ServiceCallFactory._
-import de.upb.cs.uc4.shared.server.messages.{ Accepted, Confirmation, Rejected }
 import de.upb.cs.uc4.user.api.UserService
 import play.api.Environment
-import slick.lifted.Functions.database
+import play.api.libs.json.Json
 
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.concurrent.duration._
-import scala.concurrent.{ Await, ExecutionContext, Future, TimeoutException }
 
 /** Implementation of the ReportService */
 class ReportServiceImpl(
@@ -43,7 +44,44 @@ class ReportServiceImpl(
   lazy val validationTimeout: FiniteDuration = config.getInt("uc4.timeouts.validation").milliseconds
 
   /** Request collection of all data for the given user */
-  override def prepareUserData(username: String): ServiceCall[NotUsed, Done] = ???
+  override def prepareUserData(username: String): ServiceCall[NotUsed, Done] = identifiedAuthenticated(AuthenticationRole.All :_*) {
+    (authUsername, role) =>
+      ServerServiceCall { (header, _) =>
+
+        if(authUsername != username) {
+          throw UC4Exception.OwnerMismatch
+        }
+
+        val userFuture = userService.getUser(username).handleRequestHeader(addAuthenticationHeader(header)).invoke()
+        val certificateFuture = certificateService.getCertificate(username).handleRequestHeader(addAuthenticationHeader(header)).invoke()
+        val enrollmentIdFuture = certificateService.getEnrollmentId(username).handleRequestHeader(addAuthenticationHeader(header)).invoke()
+        val encryptedPrivateKeyFuture = certificateService.getPrivateKey(username).handleRequestHeader(addAuthenticationHeader(header)).invoke()
+        var matriculationFuture : Future[ImmatriculationData] = Future.successful(null)
+        var coursesFuture: Future[Seq[Course]] = Future.successful(Nil)
+
+        role match {
+          case AuthenticationRole.Admin =>
+          case AuthenticationRole.Student =>
+            matriculationFuture = matriculationService.getMatriculationData(username).handleRequestHeader(addAuthenticationHeader(header)).invoke()
+          case AuthenticationRole.Lecturer =>
+            coursesFuture = courseService.getAllCourses(lecturerId = Some(username)).handleRequestHeader(addAuthenticationHeader(header)).invoke()
+        }
+
+        for {
+          user <- userFuture
+          certificate <- certificateFuture
+          enrollmentId <- enrollmentIdFuture
+          encryptedPrivateKey <- encryptedPrivateKeyFuture
+          immatriculationData <- matriculationFuture
+          courses <- coursesFuture
+        } yield {
+          val array = Json.toBytes(Json.toJson(user))
+
+        }
+
+        Future.successful(ResponseHeader(200, MessageProtocol.empty, List()), Done)
+      }
+  }
 
   /** Get all data for the specified user */
   override def getUserData(username: String): ServiceCall[NotUsed, Array[Byte]] = ???
