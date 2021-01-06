38e1534c511252e19c8f089e13f88be4920035b4
Luca Schäfers <lrschaef@mail.uni-paderborn.de>
2021-01-06 13:36:41 +0100

    Redeploy matriculation

--- product_code/project/Version.scala
+++ product_code/project/Version.scala
@@ -9,7 +9,7 @@ object Version {
     "course_service" -> "v0.14.1",
     "examreg_service" -> "v0.15.0",
     "hyperledger_api" -> "0.14.5",
-    "matriculation_service" -> "v0.14.2",
+    "matriculation_service" -> "v0.15.1",
     "user_service" -> "v0.14.1"
   )
 
