e70a95bfe6718c43520cb0ecfb992e31713efd4f
Luca Schäfers <lrschaef@mail.uni-paderborn.de>
2021-02-08 12:59:54 +0100

    Add regexes and error messages to ValidationConfiguration and adapt ExamResultEntry validation to actually use regexes.

--- product_code/exam_result_service/api/src/main/scala/de/upb/cs/uc4/examresult/model/ExamResultEntry.scala
+++ product_code/exam_result_service/api/src/main/scala/de/upb/cs/uc4/examresult/model/ExamResultEntry.scala
@@ -1,6 +1,6 @@
 package de.upb.cs.uc4.examresult.model
 
-import de.upb.cs.uc4.shared.client.configuration.ConfigurationCollection
+import de.upb.cs.uc4.shared.client.configuration.{ ConfigurationCollection, ErrorMessageCollection, RegexCollection }
 import de.upb.cs.uc4.shared.client.exceptions.SimpleError
 import play.api.libs.json.{ Format, Json }
 
@@ -17,13 +17,16 @@ case class ExamResultEntry(enrollmentId: String, examId: String, grade: String) 
     * @return Filled Sequence of [[SimpleError]]
     */
   def validate(implicit ec: ExecutionContext): Future[Seq[SimpleError]] = Future {
+    val nonEmptyRegex = RegexCollection.Commons.nonEmptyCharRegex
+    val nonEmptyMessage = ErrorMessageCollection.Commons.nonEmptyCharRegex
+
     var errors = List[SimpleError]()
 
-    if (enrollmentId.isEmpty) {
-      errors :+= SimpleError("enrollmentId", "EnrollmentId must not be empty.")
+    if (!nonEmptyRegex.matches(enrollmentId)) {
+      errors :+= SimpleError("enrollmentId", nonEmptyMessage)
     }
-    if (examId.isEmpty) {
-      errors :+= SimpleError("examId", "ExamId must not be empty.")
+    if (!nonEmptyRegex.matches(examId)) {
+      errors :+= SimpleError("examId", nonEmptyMessage)
     }
     if (!ConfigurationCollection.grades.contains(grade)) {
       errors :+= SimpleError("grade", "Grade must be one of " + ConfigurationCollection.grades.reduce((a, b) => a + ", " + b) + ".")
