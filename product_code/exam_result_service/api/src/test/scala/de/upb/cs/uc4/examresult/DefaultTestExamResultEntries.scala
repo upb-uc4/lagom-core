3d7893fa9e802a11e574a1f849490276e0c9a2ef
david-buderus <dbuderus@mail.uni-paderborn.de>
2021-03-02 14:04:54 +0100

    Update product_code/exam_result_service/api/src/test/scala/de/upb/cs/uc4/examresult/DefaultTestExamResultEntries.scala
    Co-authored-by: dhelmert <52128028+dhelmert@users.noreply.github.com>

--- product_code/exam_result_service/api/src/test/scala/de/upb/cs/uc4/examresult/DefaultTestExamResultEntries.scala
+++ product_code/exam_result_service/api/src/test/scala/de/upb/cs/uc4/examresult/DefaultTestExamResultEntries.scala
@@ -9,15 +9,15 @@ trait DefaultTestExamResultEntries extends DefaultTestExams with DefaultTestUser
 
   //Test exam entries
   var exam0ResultEntry0: ExamResultEntry = ExamResultEntry(student0.username + "enrollmentId", exam0.examId, ConfigurationCollection.grades.head)
-  var exam0ResultEntry1: ExamResultEntry = ExamResultEntry(student1.username + "enrollmentId", exam0.examId, ConfigurationCollection.grades.apply(1))
-  var exam0ResultEntry2: ExamResultEntry = ExamResultEntry(student2.username + "enrollmentId", exam0.examId, ConfigurationCollection.grades.apply(2))
+  var exam0ResultEntry1: ExamResultEntry = ExamResultEntry(student1.username + "enrollmentId", exam0.examId, ConfigurationCollection.grades(1))
+  var exam0ResultEntry2: ExamResultEntry = ExamResultEntry(student2.username + "enrollmentId", exam0.examId, ConfigurationCollection.grades(2))
 
   var exam1ResultEntry0: ExamResultEntry = ExamResultEntry(student0.username + "enrollmentId", exam1.examId, ConfigurationCollection.grades.head)
-  var exam1ResultEntry1: ExamResultEntry = ExamResultEntry(student1.username + "enrollmentId", exam1.examId, ConfigurationCollection.grades.apply(1))
-  var exam1ResultEntry2: ExamResultEntry = ExamResultEntry(student2.username + "enrollmentId", exam1.examId, ConfigurationCollection.grades.apply(2))
+  var exam1ResultEntry1: ExamResultEntry = ExamResultEntry(student1.username + "enrollmentId", exam1.examId, ConfigurationCollection.grades(1))
+  var exam1ResultEntry2: ExamResultEntry = ExamResultEntry(student2.username + "enrollmentId", exam1.examId, ConfigurationCollection.grades(2))
 
   var exam3ResultEntry0: ExamResultEntry = ExamResultEntry(student0.username + "enrollmentId", exam3.examId, ConfigurationCollection.grades.head)
-  var exam3ResultEntry1: ExamResultEntry = ExamResultEntry(student1.username + "enrollmentId", exam3.examId, ConfigurationCollection.grades.apply(1))
-  var exam3ResultEntry2: ExamResultEntry = ExamResultEntry(student2.username + "enrollmentId", exam3.examId, ConfigurationCollection.grades.apply(2))
+  var exam3ResultEntry1: ExamResultEntry = ExamResultEntry(student1.username + "enrollmentId", exam3.examId, ConfigurationCollection.grades(1))
+  var exam3ResultEntry2: ExamResultEntry = ExamResultEntry(student2.username + "enrollmentId", exam3.examId, ConfigurationCollection.grades(2))
 
 }
